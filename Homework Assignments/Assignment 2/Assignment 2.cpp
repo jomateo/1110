65b4e1f0c2cece2311cd6612c6d27cc3bdf4855b
Johanny <jomateo@users.noreply.github.com>
2016-12-11 00:00:07 -0500

    fixed identation

--- Homework Assignments/Assignment 2/Assignment 2.cpp
+++ Homework Assignments/Assignment 2/Assignment 2.cpp
@@ -24,72 +24,74 @@ using namespace std;
 
 int main ()
 {
-	//What does this program do?
-	string purpose = "Given its value, this program calculates the property tax of a property.";
-
-	float     value = 128000.00,
-			assesssedValue,
-			taxAmount;
-
-	//Multiply the value by 57% to find the assessed value
-	assesssedValue = value * .57;
-
-	//To find the tax amount, divide the assessedValue by 100 (each $100 is taxed by 64 cents) and multiply the answer by 64 cents.
-	taxAmount = (assesssedValue/100.00)*.64;
-
-	cout	<< purpose << endl << endl
-          << "The property's value is " << value << ".\n"
-          << "Its assessed value is " << assesssedValue << ".\n"
-          << "The tax amount is " << taxAmount << ".\n\n";
-
-     //New value for property.
-	value = 256000.00,
-	assesssedValue = value * .57;
-	taxAmount = (assesssedValue/100.00)*.64;
-
-	cout	<< "The new property's value is " << value << ".\n"
-          << "Its new assessed value is " << assesssedValue << ".\n"
-          << "The new tax amount is " << taxAmount << ".\n\n";
-
-	return 0;
+    //What does this program do?
+    string purpose =
+    "Given its value, this program calculates the property tax of a property.";
+    
+    float   value = 128000.00,
+            assesssedValue,
+            taxAmount;
+    
+    //Multiply the value by 57% to find the assessed value
+    assesssedValue = value * .57;
+    
+    //To find the tax amount, divide the assessedValue by 100 (each $100 is
+    //taxed by 64 cents) and multiply the answer by 64 cents.
+    taxAmount = (assesssedValue/100.00)*.64;
+    
+    cout    << purpose << endl << endl
+            << "The property's value is "   << value << ".\n"
+            << "Its assessed value is "     << assesssedValue << ".\n"
+            << "The tax amount is "         << taxAmount << ".\n\n";
+    
+    //New value for property.
+    value           = 256000.00,
+    assesssedValue  = value * .57;
+    taxAmount       = (assesssedValue/100.00)*.64;
+    
+    cout	<< "The new property's value is "   << value << ".\n"
+            << "Its new assessed value is "     << assesssedValue << ".\n"
+            << "The new tax amount is "         << taxAmount << ".\n\n";
+    
+    return 0;
 } //end main
 
 /*
  Memory Trace:
-
+ 
 	value
 	128000
 	256000
-
+ 
 	assessedValue
 	72960
 	145920
-
+ 
 	taxAmount
 	466.944
 	933.888
-
-
+ 
+ 
  Handcheck:
-
+ 
 	value = 128,000
-
+ 
 	assessedValue	= 128,000*.57
-                    = 72,960
-
+ = 72,960
+ 
 	taxAmount	= (72,960/100)*.64
-               = 729.60*.64
-               = 466.944
-
+ = 729.60*.64
+ = 466.944
+ 
 	New Value:
-
+ 
 	value = 256,000
-
+ 
 	assessedValue	= 256,000*.57
-                    = 145,920
-
+ = 145,920
+ 
 	taxAmount	= (145,920/100)*.64
-               = 1,459.20*.64
-               = 933.88
-
-*/
+ = 1,459.20*.64
+ = 933.88
+ 
+ */
