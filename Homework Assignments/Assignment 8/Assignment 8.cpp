64188c1cfeea4d3dfa09a97160d38b06951c8c26
Johanny <jomateo@users.noreply.github.com>
2016-12-22 23:31:20 -0500

    Completed program
    In input file:
    Changed product id num, prices, and quantities
    
    Added output window
    
    Added findphonename, printphone, and searchprice functions
    Fixed readdata and printdb function
    
    Set precision and width to output.
    
    Sorted printdb function by product number, descending, selection sort

--- Homework Assignments/Assignment 8/Assignment 8.cpp
+++ Homework Assignments/Assignment 8/Assignment 8.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
 ifstream inputFile;
@@ -26,53 +27,154 @@ public:
     
 };
 
-const int SIZE = 100;
+const int SIZE = 10;
 
-//reads in phone data from file. Accepts array and nubmer of elements filled.
-void readdata (Phone &, int);
-
-//displays phone data. Accepts array and number of elemnts filled
-void printdb (Phone, int);
+//reads in phone data from file. Accepts array and nubmer of elements filled
+void readdata (Phone [], int);
+//displays phone data. Accepts array and number of elements filled
+//sorts by productnum in descending order (selection)
+void printdb (Phone [], int);
+//finds phone name
+int findphonename (Phone [], int, string);
+//prints phone info
+void printPhone (Phone [], int);
+//search for price range and sort in ascending order (bubble)
+void searchPrice (Phone [], int);
 
 int main ()
 {
     Phone phoneInfo [SIZE];
-    int elts = 10;  //offering a choice of 10 different phones
+    int elts = 10,  //offering a choice of 10 different phones
+        pos;        //position of item found
+    string search;
     
-    readdata (phoneInfo, elts);
+    inputFile.open("Assignment 8 Input File.txt");
+    
+    if (!inputFile)
+    {
+        cout << "Input file could not be opened.\n";
+        exit (1);
+    }
+    
+    readdata(phoneInfo, elts);
     printdb(phoneInfo, elts);
+    cout << endl;
     
+    cout << "Which phone do you want to search for? ";
+    cin >> search;
+    pos = findphonename(phoneInfo, elts, search);
+    while (pos == -1)
+    {
+        cout << "Phone name not found. Try again: ";
+        cin >> search;
+        pos = findphonename(phoneInfo, elts, search);
+    }
+    printPhone(phoneInfo, pos);
+    
+    cout << endl;
+    searchPrice (phoneInfo, elts);
+    
+    inputFile.close();
     return 0;
 }
 
-void readdata (Phone arr, int n)
+void readdata (Phone arr[], int n)
 {
-    inputFile.open("Assignment 8 Input File.txt");
+    for (int i = 0; i<n; i++)
+    {
+        inputFile   >> arr[i].productnumber >> arr[i].price >> arr[i].quantity
+                    >> arr[i].name.companyName >> arr[i].name.phoneName;
+    }
+    return;
+}
+
+ void printdb (Phone arr[], int n)
+{
+    int temp;
     
-    for (int i = 0; i < n; i++)
+    //sort phone info by product number in descending order (selection sort)
+    for (int i = 0; i<n-1; i++)
     {
-        inputFile   >> arr.productnumber >> arr.price >> arr.quantity
-                    >> arr.name.companyName >> arr.name.phoneName;
+        for (int j = i+1; j<n; j++)
+        {
+            if (arr[i].productnumber < arr[j].productnumber)
+            {
+                temp = arr[j].productnumber;
+                arr[j].productnumber = arr[i].productnumber;
+                arr[i].productnumber = temp;
+            }
+        }
+    }
+    
+    //print out phone info
+    for (int i = 0; i<n; i++)
+    {
+        cout    << fixed << setprecision(2) << setw(5) << arr[i].productnumber
+                << setw(8) << arr[i].price << setw(6) << arr[i].quantity
+                << setw(8) << arr[i].name.companyName << setw(8)
+                << arr[i].name.phoneName << endl;
     }
     return;
 }
 
- void printdb (Phone arr, int n)
+int findphonename (Phone arr[], int n, string find)
 {
     for (int i = 0; i < n; i++)
-    {
-        cout    << arr.productnumber << ' ' << arr.price << ' ' << arr.quantity
-                << ' ' << arr.name.companyName << ' ' << arr.name.phoneName
-                << endl;
-    }
+        if (find == arr[i].name.phoneName)
+        {
+            cout << "Phone found at location " << i << ":\n";
+            return i;
+        }
+    return -1;
+}
+
+void printPhone (Phone arr[], int loc)
+{
+    cout    << fixed << setprecision(2) << setw(5) << arr[loc].productnumber
+            << setw(8) << arr[loc].price << setw(6) << arr[loc].quantity
+            << setw(8) << arr[loc].name.companyName << setw(8)
+            << arr[loc].name.phoneName << endl;
     return;
 }
 
-
-
-
-
-
-
-
-
+void searchPrice (Phone arr[], int n)
+{
+    double min, max, temp;
+    bool swapped;
+    int passes=0;
+    
+    //sort phones by price in ascending order (bubble sort)
+    do
+    {
+        passes++;
+        swapped = false;
+        for (int i = 0; i<n-passes; i++)
+        {
+            if (arr[i].price > arr[i+1].price)
+            {
+                temp = arr[i].price;
+                arr[i].price = arr[i+1].price;
+                arr[i+1].price = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    
+    cout << "Enter minimum price to search for: ";
+    cin >> min;
+    cout << "Enter maximum price to search for: ";
+    cin >> max;
+    //print out phones that are in between those two prices, inclusive
+    for (int i = 0; i<n; i++)
+    {
+        if (arr[i].price >= min && arr[i].price <= max)
+        {
+            cout    << fixed << setprecision(2) << setw(5)
+                    << arr[i].productnumber << setw(8) << arr[i].price
+                    << setw(6) << arr[i].quantity << setw(8)
+                    << arr[i].name.companyName << setw(8)
+                    << arr[i].name.phoneName << endl;
+        }
+    }
+    return;
+}
